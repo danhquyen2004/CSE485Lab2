945709f4ef645d5617e7bea039cdaa0f8895bf77
Rim1a <162658457+Rim1a@users.noreply.github.com>
2024-12-05 12:08:47 +0700

    Update edit.php

--- tlunews/views/admin/category/edit.php
+++ tlunews/views/admin/category/edit.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
+    <title>Category</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -31,8 +31,8 @@
                     <tr>
                         <th scope="row"><?= $category->getId(); ?></th>
                         <td><?= $category->getName(); ?></td>
-                        <td><a href="<?= DOMAIN . 'views/admin/user/edit.php?id='.$category->getId(); ?>">Edit</a></td>
-                        <td><a href="<?= DOMAIN . 'views/admin/user/remove.php?id='.$category->getId(); ?>">Remove</a></td>
+                        <td><a href="<?= DOMAIN . 'views/admin/category/edit.php?id='.$category->getId(); ?>">Edit</a></td>
+                        <td><a href="<?= DOMAIN . 'views/admin/category/remove.php?id='.$category->getId(); ?>">Remove</a></td>
                     </tr>
                     <?php
                 }
